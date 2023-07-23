dc848cfb68a17986fe81afca147108928196f0c0
unknown <kotbekareem74@gmail.com>
2023-07-23 15:31:25 +0300

    make route for test api access

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Client\RegisterController as ClientRegisterController;
 
 
 Route::group(['middleware' => 'api_password_middleware'], function () {
+    Route::get('/test', function () {
+        return 'done';
+    });
     Route::group(['prefix' => '/client'], function () {
         Route::post('/login', [ClientRegisterController::class, 'jwtLogin']);
     });
